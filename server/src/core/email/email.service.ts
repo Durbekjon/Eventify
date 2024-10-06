b17d89c7b4d2814262ad32a36bd56c91c84239d7
Durbek <saydaliyevdurbek0512@gmail.com>
2024-10-06 09:13:35 +0500

    secure false

--- server/src/core/email/email.service.ts
+++ server/src/core/email/email.service.ts
@@ -7,7 +7,7 @@ export class EmailService {
   private transporter = createTransport({
     host: process.env.EMAIL_HOST,
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
