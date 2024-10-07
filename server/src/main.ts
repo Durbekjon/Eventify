f52bbddcfba885568690fbfc33a4aeee1c7b9ffe
Durbek <saydaliyevdurbek0512@gmail.com>
2024-10-07 11:52:18 +0500

    static public

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { APP_MODE } from '@consts/app-mode'
+import * as serveStatic from 'serve-static'
+import { join } from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import * as express from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors()
   app.enableVersioning()
 
@@ -24,9 +28,16 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config)
 
-  if(APP_MODE === 'development'){
+  app.use(serveStatic(join(__dirname, '..', 'public')))
+
+  if (APP_MODE === 'development') {
     SwaggerModule.setup('public/docs', app, document)
   }
+
+  // Fallback route to serve index.html for any unmatched routes
+  app.use('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+  })
 
   await app.listen(4000)
 }
