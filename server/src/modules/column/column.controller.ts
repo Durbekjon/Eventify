476e7014e0b24281a668751d20de9a5afcb4d72c
Durbek <saydaliyevdurbek0512@gmail.com>
2024-10-11 16:32:45 +0500

    column response

--- server/src/modules/column/column.controller.ts
+++ server/src/modules/column/column.controller.ts
@@ -8,13 +8,23 @@ import {
   UseGuards,
 } from '@nestjs/common'
 import { ColumnService } from './column.service'
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 import { JwtAuthGuard } from '@guards/jwt-auth.guard'
 import { CreateColumnDto } from './dto/create-column.dto'
 import { User } from '@decorators/user.decorator'
 import { IUser } from '@user/dto/IUser'
 import { Column } from '@prisma/client'
 import { UpdateColumnDto } from './dto/update-column.dto'
+import {
+  CreateColumnResponseDto,
+  DeleteColumnResponseDto,
+  UpdateColumnResponseDto,
+} from './dto/column-response.dto'
 
 @ApiBearerAuth()
 @ApiTags('Column')
@@ -24,16 +34,15 @@ export class ColumnController {
   constructor(private readonly service: ColumnService) {}
 
   @Post()
+  @ApiResponse({ type: () => CreateColumnResponseDto })
   @ApiOperation({ summary: 'Create column' })
-  createColumn(
-    @Body() body: CreateColumnDto,
-    @User() user: IUser,
-  ) {
+  createColumn(@Body() body: CreateColumnDto, @User() user: IUser) {
     return this.service.createColumn(body, user)
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Update column' })
+  @ApiResponse({ type: () => UpdateColumnResponseDto })
   updateColumn(
     @Param('id') id: string,
     @Body() body: UpdateColumnDto,
@@ -42,6 +51,7 @@ export class ColumnController {
     return this.service.updateColumn(user, id, body)
   }
   @Delete(':id')
+  @ApiResponse({ type: () => DeleteColumnResponseDto })
   deleteColumn(@Param('id') id: string, @User() user: IUser) {
     return this.service.deleteColumn(id, user)
   }
