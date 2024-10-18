c99ac004151c1972732507e2d5ac93e33b018a2f
Durbek <saydaliyevdurbek0512@gmail.com>
2024-10-18 13:15:38 +0500

    log for company

--- server/src/modules/company/company.module.ts
+++ server/src/modules/company/company.module.ts
@@ -4,13 +4,10 @@ import { CompanyService } from './company.service'
 import { RoleService } from '@role/role.service'
 import { PrismaService } from '@core/prisma/prisma.service'
 import { CompanyController } from './company.controller'
-import { MemberService } from '@member/member.service'
-import { MemberRepository } from '@member/member.repository'
 import { NotificationService } from '@notification/notification.service'
 import { UserService } from '@user/user.service'
 import { NotificationRepository } from '@notification/notification.repository'
 import { UserRepository } from '@user/user.repository'
-import { StripeService } from '@core/stripe/stripe.service'
 import { LogRepository } from '@log/log.repository'
 
 @Module({
@@ -20,13 +17,10 @@ import { LogRepository } from '@log/log.repository'
     CompanyRepository,
     RoleService,
     PrismaService,
-    MemberService,
-    MemberRepository,
     NotificationService,
     UserService,
     NotificationRepository,
     UserRepository,
-    StripeService,
     UserService,
     UserRepository,
     LogRepository,
--- server/src/modules/company/company.service.ts
+++ server/src/modules/company/company.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ForbiddenException,
   Injectable,
   NotFoundException,
@@ -8,22 +9,23 @@ import { CompanyRepository } from './company.repository'
 import { PrismaService } from '@core/prisma/prisma.service'
 import { IUser } from '@/modules/user/dto/IUser'
 import { RoleService } from '@role/role.service'
-import { RoleTypes } from '@prisma/client'
+import { Company, RoleTypes } from '@prisma/client'
 import { HTTP_MESSAGES } from '@consts/http-messages'
 import { UpdateCompanyDto } from './dto/update-company.dto'
-import { MemberService } from '@member/member.service'
 import { LogRepository } from '@log/log.repository'
 import { CreateLogDto } from '@log/dto/create-log.dto'
 import { LOG_MESSAGES } from '@consts/log.messages'
+import { UserService } from '@user/user.service'
+import { RoleDto } from '@role/dto/role.dto'
 
 @Injectable()
 export class CompanyService {
   constructor(
     private readonly repository: CompanyRepository,
     private readonly role: RoleService,
-    private readonly member: MemberService,
     private readonly prisma: PrismaService,
     private readonly log: LogRepository,
+    private readonly user: UserService,
   ) {}
   async create(body: CreateCompanyDto, user: IUser) {
     const company = await this.prisma.company.create({
@@ -47,21 +49,26 @@ export class CompanyService {
   }
 
   async getOne(id: string, user: IUser) {
+    await this.validateUserRole(user)
+
     const company = await this.repository.findById(id)
 
-    if (!company || company.authorId !== user.id)
-      throw new NotFoundException(HTTP_MESSAGES.COMPANY.NOT_FOUND)
+    if (!company) throw new NotFoundException(HTTP_MESSAGES.COMPANY.NOT_FOUND)
 
     return company
   }
 
   async update(id: string, body: UpdateCompanyDto, user: IUser) {
-    const company = await this.repository.findById(id)
+    await this.validateUserRole(user)
 
-    if (!company || company.authorId !== user.id)
-      throw new NotFoundException(HTTP_MESSAGES.COMPANY.NOT_FOUND)
+    const company = await this.findOne(id, user.id)
 
     const data = { name: body.name }
+
+    const logMessage = `Company name: ${company.name} changed to ${body.name}`
+
+    await this.createLog(user.id, company.id, logMessage)
+
     return await this.prisma.company.update({
       where: { id },
       data,
@@ -69,22 +76,62 @@ export class CompanyService {
   }
 
   async delete(id: string, user: IUser) {
-    const company = await this.repository.findById(id)
+    await this.validateUserRole(user)
 
-    if (!company || company.authorId !== user.id)
-      throw new NotFoundException(HTTP_MESSAGES.COMPANY.NOT_FOUND)
+    await this.findOne(id, user.id)
 
     await this.prisma.company.delete({ where: { id } })
 
-    if (company.roles.length > 0) await this.role.deleteCompanyRoles(company.id)
-    if (company.members.length > 0)
-      await this.member.deleteManyMembersByCompany(company.id)
+    await this.cleanDatas(id)
 
     return { status: 'OK' }
+  }
+
+  private async findOne(companyId: string, userId: string): Promise<Company> {
+    const company = await this.prisma.company.findUnique({
+      where: { id: companyId },
+    })
+
+    if (!company) throw new NotFoundException(HTTP_MESSAGES.COMPANY.NOT_FOUND)
+
+    if (company.authorId !== userId)
+      throw new ForbiddenException(HTTP_MESSAGES.COMPANY.NOT_AUTHOR)
+
+    return company
+  }
+
+  private async cleanDatas(companyId: string) {
+    const option = { where: { companyId } }
+    await this.prisma.$transaction([
+      this.prisma.log.deleteMany(option),
+      this.prisma.task.deleteMany(option),
+      this.prisma.member.deleteMany(option),
+      this.prisma.notification.deleteMany(option),
+      this.prisma.workspace.deleteMany(option),
+      this.prisma.sheet.deleteMany(option),
+      this.prisma.select.deleteMany(option),
+      this.prisma.column.deleteMany(option),
+      this.prisma.companySubscription.deleteMany(option),
+      this.prisma.role.deleteMany(option),
+      this.prisma.log.deleteMany(option),
+    ])
   }
 
   private async createLog(userId: string, companyId: string, message: string) {
     const logOptions: CreateLogDto = { userId, companyId, message }
     return this.log.create(logOptions)
+  }
+
+  private async validateUserRole(iUser: IUser): Promise<RoleDto> {
+    const { id } = iUser
+
+    const user = await this.user.getUser(id)
+    if (!user) throw new BadRequestException(HTTP_MESSAGES.USER.NOT_FOUND)
+
+    const role: RoleDto = await this.role.getUserSelectedRole(user)
+    if (role.type !== RoleTypes.AUTHOR)
+      throw new ForbiddenException(HTTP_MESSAGES.GENERAL.ACCESS_DENIED)
+
+    return role
   }
 }
