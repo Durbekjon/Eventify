1d76515b77db5c098af9b3262a240d89664b570f
Durbek <saydaliyevdurbek0512@gmail.com>
2024-10-21 15:45:57 +0500

    subscription module deleted

--- server/src/core/subscription/subscription.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { SubscriptionService } from './subscription.service'
-import { PrismaService } from '@core/prisma/prisma.service'
-
-@Module({
-  providers: [SubscriptionService, PrismaService],
-  exports: [SubscriptionService],
-})
-export class SubscriptionModule {}
